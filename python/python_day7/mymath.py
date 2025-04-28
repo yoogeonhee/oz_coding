a6ace9e6bd21c91a76846d5bf5afffdcddcc707f
yoogeonhee <yoogeonhee0207@naver.com>
2025-04-28 13:28:39 +0900

    database

---
+++ python/python_day7/mymath.py
@@ -0,0 +1,17 @@
+import math
+class mymath1:
+    def triangle(self, num1, num2):
+        answer = (num1*num2)/2
+        return answer
+
+    def circle(self, num1):
+        answer = (num1*num1*math.pi)
+        return answer
+
+    def cube(self, num1, num2, num3):
+        answer = num1*num2*num3
+        return answer
+
+    
+
+
